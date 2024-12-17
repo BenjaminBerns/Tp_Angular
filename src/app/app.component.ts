eabeb097349185afde10cf908582533af67a574e
Benji <benjixard@gmail.Com>
2024-12-17 14:20:33 +0100

    maj icon et routeur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { TaskListComponent } from './task-list/task-list.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskListComponent],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 import { TaskListComponent } from './task-list/task-list.component';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [{ path: 'tasks', component: TaskListComponent }];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'tasks', component: TaskListComponent}
+];
