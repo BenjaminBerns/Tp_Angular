edc52d9340b343b34a3dd79491a4e7f619e574c3
Benji <benjixard@gmail.Com>
2024-12-22 20:58:49 +0100

    graphique erreur incompréhensible

--- src/app/statistics-component/statistics-component.component.ts
+++ src/app/statistics-component/statistics-component.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { Task } from '../task.service';
 
 @Component({
   selector: 'app-statistics-component',
-  imports: [BrowserModule, // Requis pour les animations des graphiques
-    NgxGraphModule],
+  standalone: true,
+  imports: [NgxChartsModule, CommonModule, BrowserModule],
   templateUrl: './statistics-component.component.html',
   styleUrl: './statistics-component.component.css'
 })
-export class StatisticsComponentComponent {
 
+export class StatisticsComponentComponent implements OnInit {
+  tasks: Task[] = [];
+  chartData: any[] = [];
+  view: [number, number] = [700, 400]; 
+
+  ngOnInit(): void {
+    this.loadTasksFromLocalStorage();
+    this.prepareChartData();
+  }
+
+  loadTasksFromLocalStorage(): void {
+    const tasksFromStorage = localStorage.getItem('tasks');
+    if (tasksFromStorage) {
+      this.tasks = JSON.parse(tasksFromStorage);
+      console.log('Tasks loaded from localStorage:', this.tasks); 
+    } else {
+      console.log('No tasks found in localStorage');
+    }
+  }
+  
+  prepareChartData(): void {
+   const taskStats: { [key: string]: number } = this.tasks.reduce((acc, task) => {
+      const key = task.type + ' - ' + (task.completed ? 'Complété' : 'Non Complété');
+      if (!acc[key]) {
+        acc[key] = 0;
+      }
+      acc[key]++;
+      return acc;
+    }, {} as { [key: string]: number });
+    
+    this.chartData = Object.keys(taskStats).map(key => ({
+      name: key,
+      value: taskStats[key]
+    }));
+    
+    console.log('Chart Data:', this.chartData);  
+  }    
 }
